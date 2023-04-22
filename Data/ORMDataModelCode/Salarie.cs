bc0fd3e10812a1c23d64630e033225a3ffbb4bc4
King-Julian-Pool <78912385+King-Julian-Pool@users.noreply.github.com>
2023-04-22 18:09:43 +0200

    Ajoutez des fichiers projet.

---
+++ Data/ORMDataModelCode/Salarie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace AnnuaireEntreprise_2019.Data.AnnuaireEntreprise_DEV
+{
+
+    public partial class Salarie
+    {
+        public Salarie(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using AnnuaireEntreprise_2019.Data.AnnuaireEntreprise_DEV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnnuaireEntreprise_2019
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ConnectionHelper.Connect(DevExpress.Xpo.DB.AutoCreateOption.SchemaAlreadyExists, true);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Annuaire());
+        }
+    }
+}
