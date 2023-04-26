8c39815ee61160d900829862f5b7bca1023b93c6
King-Julian-Pool <78912385+King-Julian-Pool@users.noreply.github.com>
2023-04-26 16:01:54 +0200

    Message d'erreur + empêcher resize et maximize

--- ConnexionForm.Designer.cs
+++ ConnexionForm.Designer.cs
@@ -30,16 +30,20 @@ namespace AnnuaireEntreprise_2019
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label_Identifiant = new System.Windows.Forms.Label();
-            this.textBox_Identifiant = new System.Windows.Forms.TextBox();
-            this.label_MotDePasse = new System.Windows.Forms.Label();
-            this.textBox_MotDePasse = new System.Windows.Forms.TextBox();
+            this.button_Fermer = new System.Windows.Forms.Button();
             this.button_Valider = new System.Windows.Forms.Button();
+            this.textBox_MotDePasse = new System.Windows.Forms.TextBox();
+            this.label_MotDePasse = new System.Windows.Forms.Label();
+            this.textBox_Identifiant = new System.Windows.Forms.TextBox();
+            this.label_Identifiant = new System.Windows.Forms.Label();
+            this.label_Erreur = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label_Erreur);
+            this.panel1.Controls.Add(this.button_Fermer);
             this.panel1.Controls.Add(this.button_Valider);
             this.panel1.Controls.Add(this.textBox_MotDePasse);
             this.panel1.Controls.Add(this.label_MotDePasse);
@@ -48,59 +52,82 @@ namespace AnnuaireEntreprise_2019
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(358, 135);
+            this.panel1.Size = new System.Drawing.Size(434, 180);
             this.panel1.TabIndex = 0;
             // 
-            // label_Identifiant
+            // button_Fermer
             // 
-            this.label_Identifiant.AutoSize = true;
-            this.label_Identifiant.Location = new System.Drawing.Point(27, 29);
-            this.label_Identifiant.Name = "label_Identifiant";
-            this.label_Identifiant.Size = new System.Drawing.Size(69, 17);
-            this.label_Identifiant.TabIndex = 0;
-            this.label_Identifiant.Text = "Identifiant";
-            // 
-            // textBox_Identifiant
-            // 
-            this.textBox_Identifiant.Location = new System.Drawing.Point(149, 23);
-            this.textBox_Identifiant.Name = "textBox_Identifiant";
-            this.textBox_Identifiant.Size = new System.Drawing.Size(197, 22);
-            this.textBox_Identifiant.TabIndex = 1;
-            // 
-            // label_MotDePasse
-            // 
-            this.label_MotDePasse.AutoSize = true;
-            this.label_MotDePasse.Location = new System.Drawing.Point(27, 74);
-            this.label_MotDePasse.Name = "label_MotDePasse";
-            this.label_MotDePasse.Size = new System.Drawing.Size(93, 17);
-            this.label_MotDePasse.TabIndex = 2;
-            this.label_MotDePasse.Text = "Mot de passe";
-            // 
-            // textBox_MotDePasse
-            // 
-            this.textBox_MotDePasse.Location = new System.Drawing.Point(149, 74);
-            this.textBox_MotDePasse.Name = "textBox_MotDePasse";
-            this.textBox_MotDePasse.Size = new System.Drawing.Size(197, 22);
-            this.textBox_MotDePasse.TabIndex = 3;
+            this.button_Fermer.Location = new System.Drawing.Point(347, 145);
+            this.button_Fermer.Name = "button_Fermer";
+            this.button_Fermer.Size = new System.Drawing.Size(75, 23);
+            this.button_Fermer.TabIndex = 5;
+            this.button_Fermer.Text = "Fermer";
+            this.button_Fermer.UseVisualStyleBackColor = true;
+            this.button_Fermer.Click += new System.EventHandler(this.button_Click);
             // 
             // button_Valider
             // 
-            this.button_Valider.Location = new System.Drawing.Point(255, 102);
+            this.button_Valider.Location = new System.Drawing.Point(243, 145);
             this.button_Valider.Name = "button_Valider";
             this.button_Valider.Size = new System.Drawing.Size(75, 23);
             this.button_Valider.TabIndex = 4;
             this.button_Valider.Text = "Valider";
             this.button_Valider.UseVisualStyleBackColor = true;
-            this.button_Valider.Click += new System.EventHandler(this.button_Valider_Click);
+            this.button_Valider.Click += new System.EventHandler(this.button_Click);
+            // 
+            // textBox_MotDePasse
+            // 
+            this.textBox_MotDePasse.Location = new System.Drawing.Point(149, 74);
+            this.textBox_MotDePasse.Name = "textBox_MotDePasse";
+            this.textBox_MotDePasse.Size = new System.Drawing.Size(273, 22);
+            this.textBox_MotDePasse.TabIndex = 3;
+            // 
+            // label_MotDePasse
+            // 
+            this.label_MotDePasse.AutoSize = true;
+            this.label_MotDePasse.Location = new System.Drawing.Point(27, 77);
+            this.label_MotDePasse.Name = "label_MotDePasse";
+            this.label_MotDePasse.Size = new System.Drawing.Size(93, 17);
+            this.label_MotDePasse.TabIndex = 2;
+            this.label_MotDePasse.Text = "Mot de passe";
+            // 
+            // textBox_Identifiant
+            // 
+            this.textBox_Identifiant.Location = new System.Drawing.Point(149, 23);
+            this.textBox_Identifiant.Name = "textBox_Identifiant";
+            this.textBox_Identifiant.Size = new System.Drawing.Size(273, 22);
+            this.textBox_Identifiant.TabIndex = 1;
+            // 
+            // label_Identifiant
+            // 
+            this.label_Identifiant.AutoSize = true;
+            this.label_Identifiant.Location = new System.Drawing.Point(27, 26);
+            this.label_Identifiant.Name = "label_Identifiant";
+            this.label_Identifiant.Size = new System.Drawing.Size(69, 17);
+            this.label_Identifiant.TabIndex = 0;
+            this.label_Identifiant.Text = "Identifiant";
+            // 
+            // label_Erreur
+            // 
+            this.label_Erreur.AutoSize = true;
+            this.label_Erreur.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Erreur.ForeColor = System.Drawing.Color.Red;
+            this.label_Erreur.Location = new System.Drawing.Point(27, 112);
+            this.label_Erreur.Name = "label_Erreur";
+            this.label_Erreur.Size = new System.Drawing.Size(0, 15);
+            this.label_Erreur.TabIndex = 6;
             // 
             // ConnexionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(358, 135);
+            this.ClientSize = new System.Drawing.Size(434, 180);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "ConnexionForm";
-            this.Text = "ConnexionForm";
+            this.ShowInTaskbar = false;
+            this.Text = "Connexion";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -115,5 +142,7 @@ namespace AnnuaireEntreprise_2019
         private System.Windows.Forms.TextBox textBox_Identifiant;
         private System.Windows.Forms.Label label_Identifiant;
         private System.Windows.Forms.Button button_Valider;
+        private System.Windows.Forms.Button button_Fermer;
+        private System.Windows.Forms.Label label_Erreur;
     }
 }
--- ConnexionForm.cs
+++ ConnexionForm.cs
@@ -25,16 +25,34 @@ namespace AnnuaireEntreprise_2019
             this.parentMainForm = parentMainForm;
         }
 
-        private void button_Valider_Click(object sender, EventArgs e)
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (button != null)
+            {
+                switch (button.Name)
+                {
+                    case "button_Valider":
+                        this.Connexion();
+                        break;
+                    case "button_Fermer":
+                        this.Close();
+                        break;
+                }
+            }
+        }
+        private void Connexion()
         {
             string identifiant = this.textBox_Identifiant.Text;
-            string motDePasse = this.textBox_MotDePasse.Text;
+            string cryptedMotDePasse = this.textBox_MotDePasse.Text;
 
-            if (identifiant != string.Empty && motDePasse != string.Empty)
+            if (identifiant != string.Empty && cryptedMotDePasse != string.Empty)
             {
+                string decryptedMotDePasse = this.Decrypt(cryptedMotDePasse);
 
                 BinaryOperator identifiantOp = new BinaryOperator(nameof(Utilisateur.Identifiant), identifiant);
-                BinaryOperator motDePasseOp = new BinaryOperator(nameof(Utilisateur.MotDePasse), motDePasse);
+                BinaryOperator motDePasseOp = new BinaryOperator(nameof(Utilisateur.MotDePasse), decryptedMotDePasse);
 
                 Utilisateur utilisateur = session.FindObject<Utilisateur>(CriteriaOperator.And(identifiantOp, motDePasseOp));
 
@@ -42,8 +60,6 @@ namespace AnnuaireEntreprise_2019
                 {
                     if (utilisateur != null)
                     {
-                        this.button_Valider.ForeColor = Color.Green;
-
                         parentMainForm.utilisateurIdentifiant = utilisateur.Identifiant;
                         parentMainForm.modeAdmin = utilisateur.Admin;
                         parentMainForm.connecte = true;
@@ -51,11 +67,23 @@ namespace AnnuaireEntreprise_2019
                     }
                     else
                     {
-                        this.button_Valider.ForeColor = Color.Red;
-                        parentMainForm.connecte = false;
+                        this.label_Erreur.Text = "Identifiant ou mot de passe incorrect";
                     }
                 }
             }
+            else
+            {
+                this.label_Erreur.Text = "Veuillez renseigner un identifiant et un mot de passe";
+            }
+        }
+
+        private string Decrypt(string cryptedPassword)
+        {
+            string decryptedPassword = null;
+
+
+
+            return decryptedPassword;
         }
     }
 }
